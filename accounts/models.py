e8b8590b0532dd377222910c06e33d9b3c78e4bc
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2023-10-25 08:05:30 +0400

    Create models.py in accounts app

---
+++ accounts/models.py
@@ -0,0 +1,99 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+# Create your models here.
+class MyAccountManager(BaseUserManager):
+    def create_user(self, first_name, last_name, username, email, password=None):
+        if not email:
+            raise ValueError('User must have an email address')
+
+        if not username:
+            raise ValueError('User must have a username')
+
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, first_name, last_name, username, email, password):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username = username,
+            password = password,
+            first_name = first_name,
+            last_name = last_name,
+        )
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+        return user
+
+
+COUNTRIES = (
+    ('us', 'United States'),
+    ('fr', 'France'),
+    ('ru', 'Russia'),
+    ('ca', 'Canada'),
+    ('cn', 'China'),
+    ('in', 'India'),
+)
+
+class Account(AbstractBaseUser):
+    first_name      = models.CharField(max_length=50)
+    last_name       = models.CharField(max_length=50)
+    username        = models.CharField(max_length=50, unique=True)
+    email           = models.EmailField(max_length=100, unique=True)
+    phone_number    = models.CharField(max_length=20)
+    gender = models.BooleanField(null=True, blank=True) # true = male
+    city = models.CharField(max_length=50, null=True, blank=True)
+    country = models.CharField(max_length=2, choices=COUNTRIES, null=True, blank=True)
+
+    # required
+    date_joined     = models.DateTimeField(auto_now_add=True)
+    last_login      = models.DateTimeField(auto_now=True)
+    is_admin        = models.BooleanField(default=False)
+    is_staff        = models.BooleanField(default=False)
+    is_active       = models.BooleanField(default=False)
+    is_superadmin   = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects = MyAccountManager()
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return f'{self.username} ({self.email})'
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, add_label):
+        return True
+
+
+class UserProfile(models.Model):
+    account = models.OneToOneField(Account, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='userprofile/', blank=True, null=True)
+
+    address_line_1 = models.CharField(max_length=50, blank=True)
+    address_line_2 = models.CharField(max_length=50, blank=True)
+    country = models.CharField(max_length=50, blank=True)
+    state = models.CharField(max_length=50, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return self.account.full_name()
+
+    def full_address(self):
+        return f'{self.address_line_1} {self.address_line_2}'
