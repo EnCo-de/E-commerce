387505f289033f25e8780f7893f3a7292bc2fb7a
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2023-11-12 22:17:35 +0400

    Update urls.py in store app

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.store, name='store'), 
     path('search/', views.search, name='search'), 
+    path('submit-review/<int:product_pk>/', views.submit_review, name='submit_review'), 
     path('<slug:category_slug>/', views.store, name='products_by_category'), 
     path('<slug:category>/<slug:product>/', views.product_detail, name='product_detail'), 
 ]
